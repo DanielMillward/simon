0bc00a22ce060d6ae26cc55536daa2e645a19105
Daniel Millward <32293745+DanielMillward@users.noreply.github.com>
2023-03-06 21:50:14 -0700

    add login functionality

---
+++ play.js
@@ -0,0 +1,14 @@
+class Game {
+
+  
+    constructor() {
+      const playerNameEl = document.querySelector('.player-name');
+      playerNameEl.textContent = this.getPlayerName();
+    }
+
+    getPlayerName() {
+        return localStorage.getItem('userName') ?? 'Mystery player';
+    }
+}
+
+const game = new Game();
